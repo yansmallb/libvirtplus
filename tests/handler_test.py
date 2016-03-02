e8f501f9d12716bea818e54e4b11be2a4dcbb2a3
yansmallb <605157563@qq.com>
2016-03-02 14:56:13 +0800

    libvirtplus v0.1

--- tests/handler_test.py
+++ tests/handler_test.py
@@ -5,4 +5,5 @@ if __name__ == "__main__" :
     print libvirtplus.handler.getContainerInfoByID(2)
     print libvirtplus.handler.findContaienrIDByName("centos_65")
     print libvirtplus.handler.stopContainerByID(2)
-    #print libvirtplus.handler.startContainerByID(2)
+    print libvirtplus.handler.startContainerByID(2)
+    print libvirtplus.handler.checkID(2)
